d2425b97011549f8cc1bf92bfd9c0687b5e01f97
Louise Thomsson Erixon <31926492+Louise1987@users.noreply.github.com>
2019-01-13 12:46:05 +0100

    Initial commit

---
+++ Sources/AssemblyVersionAuto.cs
@@ -0,0 +1,8 @@
+using System.Reflection;
+
+
+
+[assembly: AssemblyCopyright("Copyright ? 2019 Episerver")]
+[assembly: AssemblyVersion("12.15.1.0")]
+[assembly: AssemblyInformationalVersion("12.15.1")]
+[assembly: AssemblyFileVersion("12.15.1.3487")]
