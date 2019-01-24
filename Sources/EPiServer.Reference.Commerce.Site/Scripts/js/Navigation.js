6c8c79bfebb111c58a787d1370a3a23e35bc0a2b
Louise Thomsson Erixon <31926492+Louise1987@users.noreply.github.com>
2019-01-24 23:35:26 +0100

    Navigation
    Bygga ut navigation

--- Sources/EPiServer.Reference.Commerce.Site/Scripts/js/Navigation.js
+++ Sources/EPiServer.Reference.Commerce.Site/Scripts/js/Navigation.js
@@ -3,7 +3,7 @@
         $('.navbar').on('show.bs.collapse', function () {
             var actives = $(this).find('.collapse.in'),
                 hasData;
-
+            
             if (actives && actives.length) {
                 hasData = actives.data('collapse')
                 if (hasData && hasData.transitioning) return
